ba4d8402b8b1f241a61114c0ea8ed55f61f2b320
Michael Frister <michael.frister@barzahlen.de>
2016-11-11 16:57:14 +0100

    Port GCRA algorithm from Go
    Original:
    https://github.com/throttled/throttled/blob/e9d2d96facf2f48a9c26eed8f930f667008c361e/rate.go

---
+++ lib/gcra/rate_limiter.rb
@@ -0,0 +1,100 @@
+module GCRA
+  RateLimitInfo = Struct.new(
+    :limit,
+    :remaining,
+    :reset_after,
+    :retry_after
+  )
+
+  class StoreUpdateFailed < RuntimeError; end
+
+  class RateLimiter
+    MAX_ATTEMPTS = 10
+    NANO_SECOND = 1_000_000_000
+
+    # rate_period in seconds
+    def initialize(store, rate_period, max_burst)
+      @store = store
+      # Convert from seconds to nanoseconds. Ruby's time types return floats from calculations,
+      # which is not what we want. Also, there's no proper type for durations.
+      @emission_interval = (rate_period * NANO_SECOND).to_i
+      @delay_variation_tolerance = @emission_interval * (max_burst + 1)
+      @limit = max_burst + 1
+    end
+
+    def limit(key, quantity)
+      i = 0
+
+      while i < MAX_ATTEMPTS
+        # tat refers to the theoretical arrival time that would be expected
+        # from equally spaced requests at exactly the rate limit.
+        tat_from_store, now = @store.get_with_time(key)
+
+        tat = if tat_from_store.nil?
+                now
+              else
+                tat_from_store
+              end
+
+        increment = quantity * @emission_interval
+
+        # new_tat describes the new theoretical arrival if the request would succeed.
+        # If we get a `tat` in the past (empty bucket), use the current time instead. Having
+        # a delay_variation_tolerance >= 1 makes sure that at least one request with quantity 1 is
+        # possible when the bucket is empty.
+        new_tat = [now, tat].max + increment
+
+        allow_at_and_after = new_tat - @delay_variation_tolerance
+        if now < allow_at_and_after
+
+          info = RateLimitInfo.new
+          info.limit = @limit
+
+          # Bucket size in duration minus time left until TAT, divided by the emission interval
+          # to get a count
+          # This is non-zero when a request with quantity > 1 is limited, but lower quantities
+          # are still allowed.
+          info.remaining = ((@delay_variation_tolerance - (tat - now)) / @emission_interval).to_i
+
+          # Use `tat` instead of `newTat` - we don't further increment tat for a blocked request
+          info.reset_after = (tat - now).to_f / NANO_SECOND
+
+          # There's no point in setting retry_after if a request larger than the maximum quantity
+          # is attempted.
+          if increment <= @delay_variation_tolerance
+            info.retry_after = (allow_at_and_after - now).to_f / NANO_SECOND
+          end
+
+          return true, info
+        end
+
+        # Time until bucket is empty again
+        ttl = new_tat - now
+
+        new_value = new_tat.to_i
+
+        updated = if tat_from_store.nil?
+                    @store.set_if_not_exists_with_ttl(key, new_value, ttl)
+                  else
+                    @store.compare_and_set_with_ttl(key, tat_from_store, new_value, ttl)
+                  end
+
+        if updated
+          info = RateLimitInfo.new
+          info.limit = @limit
+          info.remaining = ((@delay_variation_tolerance - ttl) / @emission_interval).to_i
+          info.reset_after = ttl.to_f / NANO_SECOND
+          info.retry_after = nil
+
+          return false, info
+        end
+
+        i += 1
+      end
+
+      raise StoreUpdateFailed.new(
+        "Failed to store updated rate limit data for key '#{key}' after #{MAX_ATTEMPTS} attempts"
+      )
+    end
+  end
+end
---
+++ spec/lib/gcra/rate_limiter_spec.rb
@@ -0,0 +1,209 @@
+require 'spec_helper'
+
+require_relative '../../../lib/gcra/rate_limiter'
+
+describe GCRA do
+  before(:all) do
+    @limit = 5
+
+    @store = TestStore.new
+    @limiter = GCRA::RateLimiter.new(@store, 1, @limit - 1)
+  end
+
+  start = 0
+
+  cases = {
+    # You can never make a request larger than the maximum
+    0 => {
+      now:           start,
+      quantity:      6,
+      exp_remaining: 5,
+      exp_reset:     0,
+      exp_retry:     nil,
+      exp_limited:   true
+    },
+    # Rate limit normal requests appropriately
+    1 => {
+      now:           start,
+      quantity:      1,
+      exp_remaining: 4,
+      exp_reset:     1.0,
+      exp_retry:     nil,
+      exp_limited:   false
+    },
+    2 => {
+      now:           start,
+      quantity:      1,
+      exp_remaining: 3,
+      exp_reset:     2 * 1.0,
+      exp_retry:     nil,
+      exp_limited:   false
+    },
+    3 => {
+      now:           start,
+      quantity:      1,
+      exp_remaining: 2,
+      exp_reset:     3 * 1.0,
+      exp_retry:     nil,
+      exp_limited:   false
+    },
+    4 => {
+      now:           start,
+      quantity:      1,
+      exp_remaining: 1,
+      exp_reset:     4 * 1.0,
+      exp_retry:     nil,
+      exp_limited:   false
+    },
+    5 => {
+      now:           start,
+      quantity:      1,
+      exp_remaining: 0,
+      exp_reset:     5 * 1.0,
+      exp_retry:     nil,
+      exp_limited:   false
+    },
+    6 => {
+      now:           start,
+      quantity:      1,
+      exp_remaining: 0,
+      exp_reset:     5 * 1.0,
+      exp_retry:     1.0,
+      exp_limited:   true
+    },
+    7 => {
+      now:           start + (3000 * 1_000_000), # +3000 milliseconds in nanoseconds
+      quantity:      1,
+      exp_remaining: 2,
+      exp_reset:     3,
+      exp_retry:     nil,
+      exp_limited:   false
+    },
+    8 => {
+      now:           start + (3100 * 1_000_000),
+      quantity:      1,
+      exp_remaining: 1,
+      exp_reset:     3.9,
+      exp_retry:     nil,
+      exp_limited:   false
+    },
+    9 => {
+      now:           start + (4000 * 1_000_000),
+      quantity:      1,
+      exp_remaining: 1,
+      exp_reset:     4.0,
+      exp_retry:     nil,
+      exp_limited:   false
+    },
+    10 => {
+      now:           start + (8000 * 1_000_000),
+      quantity:      1,
+      exp_remaining: 4,
+      exp_reset:     1.0,
+      exp_retry:     nil,
+      exp_limited:   false
+    },
+    11 => {
+      now:           start + (9500 * 1_000_000),
+      quantity:      1,
+      exp_remaining: 4,
+      exp_reset:     1.0,
+      exp_retry:     nil,
+      exp_limited:   false
+    },
+    # Zero-quantity request just peeks at the state
+    12 => {
+      now:           start + (9500 * 1_000_000),
+      quantity:      0,
+      exp_remaining: 4,
+      exp_reset:     1.0,
+      exp_retry:     nil,
+      exp_limited:   false
+    },
+    # High-quantity request uses up more of the limit
+    13 => {
+      now:           start + (9500 * 1_000_000),
+      quantity:      2,
+      exp_remaining: 2,
+      exp_reset:     3.0,
+      exp_retry:     nil,
+      exp_limited:   false
+    },
+    # Large requests cannot exceed limits
+    14 => {
+      now:           start + (9500 * 1_000_000),
+      quantity:      5,
+      exp_remaining: 2,
+      exp_reset:     3.0,
+      exp_retry:     3,
+      exp_limited:   true
+    }
+  }
+
+  # All cases are run consecutively through the same limiter
+  cases.each do |i, c|
+    it "blocks request #{i}: #{c[:exp_limited]}" do
+      @store.now = c[:now]
+      limited, info = @limiter.limit('foo', c[:quantity])
+
+      aggregate_failures do
+        expect(limited).to eq(c[:exp_limited])
+        expect(info.limit).to eq(@limit)
+        expect(info.remaining).to eq(c[:exp_remaining])
+        expect(info.reset_after).to eq(c[:exp_reset])
+        expect(info.retry_after).to eq(c[:exp_retry])
+      end
+    end
+  end
+
+  it 'raises an exception if updating the store fails' do
+    limit = 5
+    store = TestStore.new
+    store.fail_sets = true
+    limiter = GCRA::RateLimiter.new(store, 1, limit - 1)
+
+    expect {
+      limiter.limit('foo', 1)
+    }.to raise_error(
+      GCRA::StoreUpdateFailed,
+      "Failed to store updated rate limit data for key 'foo' after 10 attempts"
+    )
+  end
+
+  class TestStore
+    attr_accessor :now
+    attr_accessor :fail_sets
+
+    def initialize
+      @now = 0
+      @data = {}
+      @fail_sets = false
+    end
+
+    def get_with_time(key)
+      return @data[key], @now
+    end
+
+    def set_if_not_exists_with_ttl(key, value, ttl)
+      return false if fail_sets
+
+      if @data.has_key?(key)
+        return false
+      end
+
+      @data[key] = value
+      return true
+    end
+
+    def compare_and_set_with_ttl(key, old_value, new_value, ttl)
+      return false if fail_sets
+
+      if @data[key] != old_value
+        return false
+      end
+
+      @data[key] = new_value
+      return true
+    end
+  end
+end
